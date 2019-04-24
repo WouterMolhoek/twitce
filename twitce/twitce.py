e46a145921bc0b76de1d4a2abd91e7ace640552c
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-24 21:02:02 +0200

    Added error handler when a user does not exist

--- twitce/twitce.py
+++ twitce/twitce.py
@@ -10,8 +10,10 @@ def request_url(url, user):
         page = urllib.request.urlopen(url)
         # Parse the html
         return BeautifulSoup(page, 'html.parser')
+
     except:
-        return 'No response from Twitter.com'
+        return '404'
+
 
 # Get statistics from an user, requires a username and a stat: 'followers','following','tweets'
 def stats(user_name, stat):
@@ -28,6 +30,10 @@ def stats(user_name, stat):
         return f'Does not recognize "{stat}" as one of the enumerations'    
 
     response = request_url(twitter_url, user_name)
+
+    if response == '404':
+        return 'User does not exist'
+
     nav_item = response.find(class_='ProfileNav-item ProfileNav-item--' + stat_request)
 
     if nav_item is not None: 
@@ -53,8 +59,13 @@ def profile(user_name, option):
         return f'Does not recognize "{option}" as one of the enumerations'
     
     response = request_url(twitter_url, user_name)
+
+    if response == '404':
+        return 'User does not exist'
+        
     profile_item = response.find(class_='ProfileHeaderCard-' + option_request).get_text().strip()
+
     if profile_item == '': return 'None specified'    
     else: return profile_item    
 
-print(profile('Botv25','location'))    
+print(profile('Botv25dsfdsdsfsd','location'))    
