32437e3cfde3212fc272fc5a0a7385c586204450
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-24 19:57:56 +0200

    Added error handling when arguments are not provided

--- twitce/twitce.py
+++ twitce/twitce.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 
 twitter_url = 'https://twitter.com/'
 
-def request_url(url):
+def request_url(url, user):
     # Query the website (url) and return the html
     try:
+        url += user
         page = urllib.request.urlopen(url)
         # Parse the html
         return BeautifulSoup(page, 'html.parser')
@@ -14,19 +15,25 @@ def request_url(url):
 
 # Get statistics from a given username: (amount of followers, amount of following, amount of tweets)
 def stats(user_name, stat):
-    response = request_url(twitter_url + user_name)
     requests = ['followers','following','tweets is-active']
 
-    for req in requests:
-        if req.split()[0] == stat:
-            stat_request = req 
+    if user_name == '': return 'First argument must be provided' 
+    elif stat == '': return 'Second argument must be provided'
 
-    try:    
-        nav_item = response.find(class_='ProfileNav-item ProfileNav-item--' + stat_request)
-        if nav_item is not None: 
-            return nav_item.find(class_='ProfileNav-value').get_text().strip()
-        else:
-            return f'User doesnt have any {req.split()[0]}'    
-    except:
-        return 'User cant be found'
+    if stat in requests:
+        stat_request = stat
+    elif stat == 'tweets':
+        stat_request = requests[2]          
+    else:
+        return f'Does not recognize "{stat}" as one of the enumerations'    
 
+    response = request_url(twitter_url, user_name)
+    nav_item = response.find(class_='ProfileNav-item ProfileNav-item--' + stat_request)
+
+    if nav_item is not None: 
+        return nav_item.find(class_='ProfileNav-value').get_text().strip()
+    else:
+        return f'User doesnt have any {stat}'    
+    
+
+print(stats('realdonaldtrump','followers'))
